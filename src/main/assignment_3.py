ec8fcd4b625aa94d193fd54cd3d552691a900f82
janicept <160250783+janicept@users.noreply.github.com>
2025-03-05 08:02:21 -0500

    Update assignment_3.py

--- src/main/assignment_3.py
+++ src/main/assignment_3.py
@@ -1,62 +1,40 @@
-#Euler Method
-# Define the function for dy/dt
-def dydt(t, y):
-    return t - y**2
+# Euler Method
+def func1(t, y):  
+    return t - y**2  
 
-# Initial conditions
-t_start, y_start = 0, 1
-t_end = 2
-num_steps = 10
-step_size = (t_end - t_start) / num_steps  # h = 0.2
-
-# Lists to store results
-t_values = [t_start]
-y_values = [y_start]
+# Initial conditions for Euler Method
+t1, y1 = 0, 1  
+end_t1 = 2  
+steps1 = 10  
+h1 = (end_t1 - t1) / steps1  
 
 # Apply Euler's Method
-t_curr, y_curr = t_start, y_start
+for _ in range(steps1):
+    y1 += h1 * func1(t1, y1)  
+    t1 += h1  
 
-for _ in range(num_steps):
-    y_next = y_curr + step_size * dydt(t_curr, y_curr)  
-    t_curr += step_size 
-    
-    t_values.append(t_curr)
+print(y1)
 
-  #___________________________
-  #Runge-Kutta
-# Define the function for dy/dt
-def dydt(t, y):
-    return t - y**2
+print()  
+#__________________________
+# Runge-Kutta Method
+def func2(t, y):  
+    return t - y**2  
 
-# Initial conditions
-t_start, y_start = 0, 1
-t_end = 2
-num_steps = 10
-step_size = (t_end - t_start) / num_steps  # h = 0.2
-
-t_values = [t_start]
-y_values = [y_start]
+# Initial conditions for Runge-Kutta
+t2, y2 = 0, 1  
+end_t2 = 2  
+steps2 = 10  
+h2 = (end_t2 - t2) / steps2  
 
 # Apply Runge-Kutta 4th Order Method
-t_curr, y_curr = t_start, y_start
+for _ in range(steps2):
+    k1 = h2 * func2(t2, y2)
+    k2 = h2 * func2(t2 + h2 / 2, y2 + k1 / 2)
+    k3 = h2 * func2(t2 + h2 / 2, y2 + k2 / 2)
+    k4 = h2 * func2(t2 + h2, y2 + k3)
 
-for _ in range(num_steps):
-    k1 = step_size * dydt(t_curr, y_curr)
-    k2 = step_size * dydt(t_curr + step_size / 2, y_curr + k1 / 2)
-    k3 = step_size * dydt(t_curr + step_size / 2, y_curr + k2 / 2)
-    k4 = step_size * dydt(t_curr + step_size, y_curr + k3)
+    y2 += (1/6) * (k1 + 2*k2 + 2*k3 + k4)  
+    t2 += h2  
 
-    y_next = y_curr + (1/6) * (k1 + 2*k2 + 2*k3 + k4)  # RK4 update
-    t_curr += step_size  # Increment t
-
-    t_values.append(t_curr)
-    y_values.append(y_next)
-
-    y_curr = y_next  
-
-print(y_curr)
-    y_values.append(y_next)
-
-    y_curr = y_next  
-
-print(y_curr)
+print(y2)
